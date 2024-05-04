ad7af5fbfb1474b675ecc8b9375c3ee46b264824
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2024-05-04 23:15:48 +0330

    Add an advice counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 
 function App() {
-  const [advice, setAdvice] = useState('Click 👇🏻 to generate an advice here...');
+	const [advice, setAdvice] = useState(
+		'Click 👇🏻 to generate an advice here...'
+	);
+	const [count, setCount] = useState(0);
 
 	async function getAdvice() {
 		const res = await fetch('https://api.adviceslip.com/advice');
 		const data = await res.json();
 		setAdvice(data.slip.advice);
+		setCount((c) => ++c);
 	}
 
 	return (
 		<>
 			<h1>{advice}</h1>
 			<button onClick={getAdvice}>Get Advise</button>
+			<p>
+				You have read <strong>{count}</strong> pieces of advice.
+			</p>
 		</>
 	);
 }
