e453b9163f98d7e49731403261b7eda108e43e4a
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2024-05-04 23:11:57 +0330

    Fetch an advice from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <h1>Adviser App</h1>
+	async function getAdvice() {
+		const res = await fetch('https://api.adviceslip.com/advice');
+		const data = await res.json();
+		console.log(data.slip.advice);
+	}
+
+	return (
+		<>
+			<h1>Advise</h1>
+			<button onClick={getAdvice}>Get Advise</button>
+		</>
+	);
 }
 
 export default App;
